f089899edba97634829cac44eb56fe5db5414279
Decimo <marlonadiguemartin548@gmail.com>
2023-06-17 12:00:45 +0800

    kyu

---
+++ CodeWars/Holiday III - Fire in the boat.java
@@ -0,0 +1,11 @@
+public class CodeWars {
+	public static String fireFight(String s){
+    String replace = "";
+    
+    for(int i = 0; i < s.length(); i++){
+        replace = s.replace("Fire", "~~");
+    }
+    
+    return replace;
+  }
+}
